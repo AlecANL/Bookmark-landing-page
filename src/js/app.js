4d9e55fb327fa8a97636369f3c7c9d79e3d77b6c
alexei <alexeialvarez4@gmail.com>
2021-09-24 11:10:34 -0600

    :art: [ADD]: contact questions and featured sections

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,17 @@
-// @ts-check
-
 const $nav = document.getElementById('nav');
 const $iconMenu = document.getElementById('icon-menu');
 const $iconClose = document.getElementById('icon-close');
+const $slides = document.querySelectorAll('.slide');
+const $controls = document.querySelectorAll('.s-control');
+
+$controls.forEach((control, idx) => {
+  control.addEventListener('click', () => {
+    $controls.forEach(el => el.classList.remove('is-active'));
+    $slides.forEach(el => el.classList.remove('is-active'));
+    $slides[idx].classList.add('is-active');
+    $controls[idx].classList.add('is-active');
+  });
+});
 
 $iconMenu.addEventListener('click', () => {
   $nav.classList.add('is-active');
